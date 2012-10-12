2e48ebacc0b617d89797bfe141732d644a702f4f
knkhattar@gmail.com <knkhattar@gmail.com@0674bf8d-1630-872b-01a3-4b52c4562411>
2012-10-12 16:25:57 +0000

    Updated after testing crud operations

--- HR_ORCL_EMPLOYEE_MANAGEMENT_BODY.sql
+++ HR_ORCL_EMPLOYEE_MANAGEMENT_BODY.sql
@@ -5,7 +5,8 @@
 * functions, variables etc.
 *
 * */
-CREATE OR REPLACE
+
+create or replace
 PACKAGE BODY employee_management
 IS
   /*
@@ -13,46 +14,86 @@ IS
   * */
 FUNCTION create_employee
   (
-    last_name      VARCHAR2,
-    job_id         VARCHAR2,
-    manager_id     NUMBER,
-    salary         NUMBER,
-    commission_pct NUMBER,
-    department_id  NUMBER)
+    p_last_name     VARCHAR2,
+    p_job_id        VARCHAR2,
+    p_manager_id    NUMBER,
+    p_salary        NUMBER,
+    p_department_id NUMBER,
+    p_email         VARCHAR2,
+    p_hire_date     VARCHAR2)
   RETURN NUMBER
 IS
+  v_newemployeeid NUMBER(6,0);
 BEGIN
-  NULL;
-END hire;
+  SELECT MAX(EMPLOYEES.EMPLOYEE_ID)+1 INTO v_newemployeeid FROM employees;
+  INSERT
+  INTO EMPLOYEES
+    (
+      employee_id,
+      last_name,
+      job_id,
+      manager_id,
+      salary,
+      department_id,
+      email,
+      hire_date
+    )
+    VALUES
+    (
+      v_newemployeeid,
+      p_last_name,
+      p_job_id,
+      p_manager_id,
+      p_salary,
+      p_department_id,
+      p_email,
+      p_hire_date
+    );
+  COMMIT;
+  RETURN v_newemployeeid;
+END create_employee;
 /*Delete employee procedure */
 PROCEDURE delete_employee
   (
-    employee_id NUMBER)
+    p_employee_id NUMBER
+  )
 IS
 BEGIN
-  DBMS_OUTPUT.PUT_LINE ('Before I show you v...');
-END remove_emp;
+  DBMS_OUTPUT.PUT_LINE
+  (
+    'Before I show you v...'
+  )
+  ;
+  DELETE FROM employees WHERE employee_id = p_employee_id;
+END delete_employee;
 /*
 * Find employee function.
 * */
 FUNCTION read_employee
   (
-    employee_id NUMBER )
+    p_employee_id NUMBER )
   RETURN VARCHAR2
-IS
+                                 IS
+  v_employeename VARCHAR2 ( 20 ) := '';
 BEGIN
-  NULL;
+  SELECT employees.first_name
+  INTO v_employeename
+  FROM employees
+  WHERE employee_id = p_employee_id;
+  
+  RETURN v_employeename;
 END;
 /*
 * Update employee function.
 * */
 FUNCTION update_employee
   (
-    employee_id NUMBER,
-    salary      NUMBER )
-  RETURN NUMBER
+    p_employee_id NUMBER,
+    p_salary      NUMBER )
+  RETURN VARCHAR2
 IS
 BEGIN
-  NULL;
+  UPDATE employees SET salary = p_salary WHERE employee_id = p_employee_id ;
+  RETURN 'SUCCESS';
 END;
 END employee_management;
